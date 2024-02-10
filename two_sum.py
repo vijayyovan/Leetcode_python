fe838de8baf15660d054630b92f4b860eb300bed
Vijay Soundaram <vijay6206@gmail.com>
2024-02-09 23:27:06 -0700

    two_sum.py
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
